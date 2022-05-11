7ec72c73fab8002ed99e5df1d42138b929a6bd5a
kn8a <mydreamhomefl@gmail.com>
2022-05-11 06:30:35 -0400

    added nav styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,45 @@
 import { Link } from "react-router-dom";
+import logo from '../img/logo.png'
+import '../styles/nav.css'
+import Icon from '@mdi/react'
+import { mdiCart, mdiHome, mdiShopping } from '@mdi/js';
 
 const Nav = (props) => {
     return(
     <nav>
-        <Link to={'/home'}>Home</Link>
-        <Link to={'/products'}>Products</Link>
-        <Link to={'/Cart'}>Cart{' '+ props.qty }</Link>
+        <div>
+            <Link className='nav-logo' to={'/home'}><img alt="Store logo" src={logo}/></Link>
+        </div>
+        <div className="home-products">
+            
+                <Link className="menu-item" to={'/home'}>
+                    <Icon  className='menu-icon' path={mdiHome}
+                            title="Cart"
+                            size={1.5}
+                            color="darkcyan"
+                            />
+                    <div>Home</div>
+                </Link>
+            
+            <Link className="menu-item" to={'/products'}>
+                <Icon className="menu-icon" path={mdiShopping}
+                            title="Cart"
+                            size={1.5}
+                            color="darkcyan"
+                            />
+                Products
+            </Link>
+        </div>
+        <div>
+            <Link className="menu-item" to={'/Cart'}>
+                <Icon path={mdiCart}
+                    title="Cart"
+                    size={1.5}
+                    color="darkcyan"
+                    />
+                {' '+ props.qty + ' items' }
+            </Link>
+        </div>
 
     </nav>
 )
